0209d17acef9689a2852bffd3639e215a5f85430
fablopes <fabriciolopessilveira@gmail.com>
2023-06-28 16:57:36 -0300

    Desenvolvimento de microservicos com Nest.js

---
+++ nestjs/src/orders/orders.controller.ts
@@ -0,0 +1,32 @@
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { OrdersService } from './orders.service';
+import { InitTransactionDto, InputExecuteTransactionDto } from './order.dto';
+
+@Controller('wallets/:wallet_id/orders')
+export class OrdersController {
+  constructor(private readonly ordersService: OrdersService) {}
+
+  @Get()
+  all(@Param('wallet_id') wallet_id: string) {
+    return this.ordersService.all({ wallet_id });
+  }
+
+  @Post()
+  initTransactionDto(
+    @Param('wallet_id') wallet_id: string,
+    @Body() body: Omit<InitTransactionDto, 'wallet_id'>,
+  ) {
+    return this.ordersService.InitTransaction({
+      ...body,
+      wallet_id,
+    });
+  }
+
+  @Post('execute')
+  executeTransactionRest(
+    @Param('wallet_id') wallet_id: string,
+    @Body() body: InputExecuteTransactionDto,
+  ) {
+    return this.ordersService.executeTransaction(body);
+  }
+}
